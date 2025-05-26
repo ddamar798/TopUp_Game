d22fa9792fc326f032dcc3e11e48e811ef8f2864
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-26 18:18:30 +0700

    add main

---
+++ backend/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "log"
+    "net/http"
+    "fmt"
+)
+
+var db *gorm.DB
+
+func initDB() {
+    dsn := "host=localhost user=postgres password= dbname=TopupDB port=5432 sslmode=disable"
+    var err error
+    db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
+    fmt.Println("✅ Connected to PostgreSQL")
+}
+
+func main() {
+    initDB()
+
+    http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("pong"))
+    })
+
+    fmt.Println("🚀 Backend running at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
